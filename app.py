db3bbe000965324e409b48b547c7e3286c968320
TIWA RAMDHANI <41172181+Miharada@users.noreply.github.com>
2022-02-24 16:23:53 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 import neo4jcon as neo
-from rules import text2cipher
+from rules import text2cipher, neojarowinkler
 from IPython.display import HTML
 
 app = Flask(__name__)
@@ -21,17 +21,33 @@ def home_page():
 def generateHadith():
     text = request.form['statement']
     df = text2cipher(text.lower(), conn)
-    print(HTML(df.to_html()))
-    return render_template('QA_Hadith.html', statement=text, tableHadith=HTML(df.to_html(classes='table table-striped" id = "a_nice_table',
+    try:
+        return render_template('QA_Hadith.html', statement=text, tableHadith=HTML(df.to_html(classes='table table-striped" id = "a_nice_table',
+                                       index=False, border=0)), rules=False)
+    except:
+        return render_template('QA_Hadith.html', rules=True)
+
+@app.route('/qahadithv2', methods=['POST'])
+def generatehadithsimbased():
+    text = request.form['statement']
+    df = neojarowinkler(text.lower(), conn)
+    try:
+        return render_template('QA_v2.html', statement=text, tableHadith=HTML(df.to_html(classes='table table-striped" id = "a_nice_table',
                                        index=False, border=0)))
+    except:
+        return render_template('QA_v2.html')
 
 
 @app.route('/expertpage')
 def expertcomment():
     return render_template("ExpertPage.html")
 
+@app.route('/qahadithv2')
+def qahadith2():
+    return render_template("QA_v2.html")
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
 
 #https://neo4j.com/developer/cypher/filtering-query-results/
 #https://neo4j.com/developer/cypher/guide-build-a-recommendation-engine/
